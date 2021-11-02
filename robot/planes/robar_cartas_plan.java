65533e715ecf6fa56962c6dead2ef9912e8d4af4
JorgeSerranoP <jorgeserranoperez00@gmail.com>
2021-11-02 12:13:00 +0100

    Estructura inicial

---
+++ robot/planes/robar_cartas_plan.java
@@ -0,0 +1,21 @@
+package robot.planes;
+
+import jadex.util.SUtil;
+import robot.creencias.*;
+import jadex.runtime.IMessageEvent;
+import jadex.runtime.Plan;
+import jadex.runtime.IGoal;
+import jadex.adapter.fipa.AgentIdentifier;
+import jadex.adapter.fipa.SFipa;
+import jadex.adapter.fipa.AgentDescription;
+import jadex.adapter.fipa.SearchConstraints;
+import jadex.adapter.fipa.ServiceDescription;
+import ontologia.acciones.*;
+
+public class robar_cartas_plan extends Plan
+{
+    public void body()
+    {
+
+    }
+}
