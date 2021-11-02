a69ab3438e8863494523f576a79c2dfff16bda46
Miriam Valdizan <mivalar3@gmail.com>
2021-11-02 22:17:02 +0100

    descartar_cartas

--- robot/planes/descartar_cartas_plan.java
+++ robot/planes/descartar_cartas_plan.java
@@ -11,6 +11,7 @@ import jadex.adapter.fipa.AgentDescription;
 import jadex.adapter.fipa.SearchConstraints;
 import jadex.adapter.fipa.ServiceDescription;
 import ontologia.acciones.*;
+import ontologia.predicados.Cartas_descartadas;
 
 public class Descartar_cartas_plan extends Plan
 {
@@ -35,16 +36,16 @@ public class Descartar_cartas_plan extends Plan
 
     public void body()
     {
-        // busco el jugador (reciever)
-		ServiceDescription sd = new ServiceDescription();
-		sd.setType("jugador");
-		AgentDescription dfadesc = new AgentDescription();
-		dfadesc.addService(sd);
-		IGoal ft = createGoal("df_search");
-		ft.getParameter("description").setValue(dfadesc);
-		dispatchSubgoalAndWait(ft);
-		AgentDescription[]	result	= (AgentDescription[])ft.getParameterSet("result").getValues();
-		AgentIdentifier jugador = result[new Random().nextInt(result.length)].getName();
+		// miro cuantas cartas tiene ese jugador
+		IMessageEvent	queryif	= (IMessageEvent)getInitialEvent();
+		AgentIdentifier jugador = (AgentIdentifier) queryif.getParameter("sender").getValue();
+		Cartas_politicas_jugador[] cartas= (Cartas_politicas_jugador[]) getBeliefbase().getBeliefSet("cartas").getFacts();
+			
+		System.out.println ("jugador registra la carta del jugador...");
+		Cartas_politicas_jugador cpj= new Cartas_politicas_jugador();
+		cpj.setJugador(jugador);
+		cpj.setCartas(7);
+		getBeliefbase().getBeliefSet("cartas").addFact(cpj);
 
 		// le preguntamos si tiene mas de 6 cartas
 		System.out.println("mazo de cartas preguntando si tiene mas de 6 cartas politicas...");
@@ -52,12 +53,22 @@ public class Descartar_cartas_plan extends Plan
 		msg.setContent(cartas > 6);
 		msg.getParameterSet(SFipa.RECEIVERS).addValue(jugador);
 		sendMessage(msg);
-
-        // informamos que se ha descartado de las cartas
-        Cartas_descartadas cd = new Cartas_descartadas();
-        IMessageEvent	msg	= createMessageEvent("informdescartadas");
-		msg.setContent(cd);
-		msg.getParameterSet(SFipa.RECEIVERS).addValue(jugador);
-		sendMessage(msg);
+		if (cpj.getCartas()>6)
+		{
+			msg = createMessageEvent("confirmdescartar");
+			System.out.println("jugador confirma que tiene mas de 6 cartas");
+			 // informamos que se ha descartado de las cartas
+			 Cartas_descartadas cd = new Cartas_descartadas();
+			 IMessageEvent	msg	= createMessageEvent("informdescartadas");
+			 msg.setContent(cd);
+			 msg.getParameterSet(SFipa.RECEIVERS).addValue(jugador);
+			 sendMessage(msg);
+			 cpj.setJugador(jugador);
+			 cpj.setCartas(6);
+		}
+		else 
+		{	
+			IMessageEvent	msg	= createMessageEvent("disconfirmdescartar");
+		}   
     }
 }
